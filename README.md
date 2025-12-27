2a92bd5a7935f30bd7edc630e0a8587e5dfbd1ca
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2025-12-27 09:50:58 +0100

    Projektdateien hinzufügen.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Timer und Demo
+
+![NET](https://img.shields.io/badge/NET-10.0-green.svg)
+![License](https://img.shields.io/badge/License-MIT-blue.svg)
+![VS2022](https://img.shields.io/badge/Visual%20Studio-2026-white.svg)
+![Version](https://img.shields.io/badge/Version-1.0.2025.0-yellow.svg)]
+
+Das Beispiel soll zeigen, welche Timervarianten es unter NET 10 gibt und wozu diese am besten geeignet sind.
+
+
---
+++ TimerDemo/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MainWindow.cs" company="Lifeprojects.de">
+//     Class: MainWindow
+//     Copyright © Lifeprojects.de 2026
+// </copyright>
+//
+// <author>Gerhard Ahrens - Lifeprojects.de</author>
+// <email>developer@lifeprojects.de</email>
+// <date>29.01.2026</date>
+//
+// <summary>
+// Timervarianten unter NET 10
+// </summary>
+//-----------------------------------------------------------------------
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Ribbon;
+
+namespace TimerDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            WeakEventManager<Window, RoutedEventArgs>.AddHandler(this, "Loaded", this.OnLoaded);
+            WeakEventManager<Window, CancelEventArgs>.AddHandler(this, "Closing", this.OnWindowClosing);
+
+            this.WindowTitel = "Timervarianten unter NET 10";
+            this.DataContext = this;
+        }
+
+        private string _WindowTitel;
+
+        public string WindowTitel
+        {
+            get { return _WindowTitel; }
+            set
+            {
+                if (this._WindowTitel != value)
+                {
+                    this._WindowTitel = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            WeakEventManager<Button, RoutedEventArgs>.AddHandler(this.BtnCloseApplication, "Click", this.OnCloseApplication);
+
+        }
+
+        private void OnCloseApplication(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void OnWindowClosing(object sender, CancelEventArgs e)
+        {
+            e.Cancel = false;
+
+            MessageBoxResult msgYN = MessageBox.Show("Wollen Sie die Anwendung beenden?", "Beenden", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (msgYN == MessageBoxResult.Yes)
+            {
+                App.ApplicationExit();
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        #region INotifyPropertyChanged implementierung
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler == null)
+            {
+                return;
+            }
+
+            var e = new PropertyChangedEventArgs(propertyName);
+            handler(this, e);
+        }
+        #endregion INotifyPropertyChanged implementierung
+    }
+}
